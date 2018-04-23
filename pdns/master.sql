9c533b3b3ef5878e9b89ff9c837337231ab0b29a
iGlaedr <jannik.tom@gmail.com>
2018-04-23 09:23:31 +0200

    Update master.sql
    Added dynamic ids to the domains and records.
    Used vars for the ralation between the domain and the records.

--- pdns/master.sql
+++ pdns/master.sql
@@ -1,8 +1,10 @@
-INSERT INTO domains (`id`, name, master, last_check, type, notified_serial, `account`) VALUES (1, 'example.com', NULL, NULL, 'MASTER', NULL, NULL);
-INSERT INTO records (`id`, domain_id, name, type, content, ttl, prio, change_date, disabled, ordername, `auth`) VALUES
-(2, 1, 'example.com', 'SOA', 'ns0.example.com mail.gmail.com. 2018040600 3600 900 604800 86400', 86400, NULL, NULL, 0, NULL, 1),
-(3, 1, 'example.com', 'NS', 'ns0.example.com', 86400, 0, NULL, 0, NULL, 1),
-(4, 1, 'example.com', 'NS', 'ns1.example.com', 86400, 0, NULL, 0, NULL, 1),
-(5, 1, 'example.com', 'A', 'placeA', 86400, 0, NULL, 0, NULL, 1),
-(6, 1, 'ns0.example.com', 'A', 'placeNS0', 86400, 0, NULL, 0, NULL, 1),
-(7, 1, 'ns1.example.com', 'A', 'placeNS1', 86400, 0, NULL, 0, NULL, 1);
+use powerdns;
+INSERT INTO domains (name, master, last_check, type, notified_serial, `account`) VALUES ('example.com.com', NULL, NULL, 'MASTER', NULL, NULL);
+SELECT id from domains WHERE name='example.com.com' INTO @domain_id;
+INSERT INTO records (domain_id, name, type, content, ttl, prio, change_date, disabled, ordername, `auth`) VALUES
+(@domain_id, 'example.com.com', 'SOA', 'ns0.example.com.com mail.gmail.com. 2018040600 3600 900 604800 86400', 86400, NULL, NULL, 0, NULL, 1),
+(@domain_id, 'example.com.com', 'NS', 'ns0.example.com.com', 86400, 0, NULL, 0, NULL, 1),
+(@domain_id, 'example.com.com', 'NS', 'ns1.example.com.com', 86400, 0, NULL, 0, NULL, 1),
+(@domain_id, 'example.com.com', 'A', 'placeA', 86400, 0, NULL, 0, NULL, 1),
+(@domain_id, 'ns0.example.com.com', 'A', 'placeNS0', 86400, 0, NULL, 0, NULL, 1),
+(@domain_id, 'ns1.example.com.com', 'A', 'placeNS1', 86400, 0, NULL, 0, NULL, 1);
