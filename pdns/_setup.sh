8d71e6b4ab00f0c2a2fc88bf9bfb01a200f8ab9c
iGlaedr <jannik.tom@gmail.com>
2018-04-23 12:43:21 +0200

    Update _setup.sh
    FIX in pdns config creation master slave options

--- pdns/_setup.sh
+++ pdns/_setup.sh
@@ -2,14 +2,15 @@
 
 #Get configuration from user
 echo 	"Script to setup powrdns [master|slave] nameserver"
-read -p "Pleas enter the master IP: " masterip
-read -p "Pleas enter the slave IP: " slaveip
+read -p "Pleas enter the master IPv4: " masterip
+read -p "Pleas enter the slave IPv4: " slaveip
 read -p "Pleas enter the domain: " domain
 read -p "Pleas enter the A Records IP: " ARecord
 read -p "Pleas enter the your new MYSQL Root password:  " -r -s MYSQL_ROOT_PASSWORD && echo ""
 read -p "Pleas enter the your new DB user: " MYSQL_DB_USER
 read -p "Pleas enter the the new password for the DB user: " -r -s MYSQL_DB_PASSWORD && echo ""
-read -p "Pleas enter the the server typre [master|slave]("0" for aborting zone creation): " type
+read -p "Pleas enter the the server typre [master|slave]" type
+read -p "Do you want to create a basic IPv4 zone [yes|no]" zonev4 
 
 # Install expect needed for interaction between script an UIs
 apt update && apt -y install expect
@@ -57,50 +58,68 @@ echo "$SECURE_MYSQL"
 apt -y purge expect
 apt -y autoremove 
 
-# Move original conf to backup
+# Move original config to backup
 mv /etc/powerdns/pdns.conf /etc/powerdns/_orgi.pdns.conf
 # Create config files
 cp ./pdns.conf /etc/powerdns/pdns.conf
 cp ./pdns.gmysql.conf /etc/powerdns/pdns.d/pdns.gmysql.conf 
 
-# Edit server config 
-sed -i -e "s/1.1.1.1/$slaveip/g" /etc/powerdns/pdns.conf
+
+
+while [ $type !=  "0" ]
+do
+	if [ $type == "master" ]
+	then
+        sed -i -e "s/1.1.1.1/$slaveip/g" /etc/powerdns/pdns.conf
+        sed -i -e "s/slave=yes/slave=no/g" /etc/powerdns/pdns.conf
+	elif [ $type == "slave" ]
+	then
+        sed -i -e "s/1.1.1.1/0.0.0.0/g" /etc/powerdns/pdns.conf
+        sed -i -e "s/master=yes/master=no/g" /etc/powerdns/pdns.conf
+	else
+		echo "Wronge server type (`0` for aborting server configuration) or"
+		read -p "Pleas enter the the server typre [master|slave]: " type
+	fi
+done
+
 # Edit gmysql config
 sed -i -e "s/powerdns_user/$MYSQL_DB_USER/g" /etc/powerdns/pdns.conf
 sed -i -e "s/powerdns_user_password/$MYSQL_DB_PASSWORD/g" /etc/powerdns/pdns.conf
+
 
 # Create database
 mysql -u root -p$MYSQL_ROOT_PASSWORD < ./schema.sql
 
 # Create zone
-while [ $type !=  "0" ]
-do
-	if [ $type == "master" ]
-	then
-		# Edit Zone
-		sed -i -e "s/example.com/$domain/g" ./master.sql
-		sed -i -e "s/placeNS0.com/$masterip/g" ./master.sql
-		sed -i -e "s/placeNS1.com/$slaveip/g" ./master.sql
-		sed -i -e "s/placeA/$ARecord/g" ./master.sql
-		# Create Zone
-		mysql -u root -$MYSQL_ROOT_PASSWORD < ./master.sql
-		# Reset Zone
-		sed -i -e "s/$domain/example.com/g" ./master.sql
-		sed -i -e "s/$masterip/placeNS0.com/g" ./master.sql
-		sed -i -e "s/$slaveip/placeNS1.com/g" ./master.sql
-		sed -i -e "s/$ARecord/placeA/g" ./master.sql
-	elif [ $type == "slave" ]
-	then
-		# Edit Zone
-		sed -i -e "s/example.com/$domain/g" ./slave.sql
-		sed -i -e "s/placeNS0.com/$masterip/g" ./master.sql
-		# Create Zone
-		mysql -u root -$MYSQL_ROOT_PASSWORD < ./slave.sql
-		# Reset Zone
-		sed -i -e "s/$domain/example.com/g" ./slave.sql
-		sed -i -e "s/$masterip/placeNS0.com/g" ./master.sql
-	else
-		echo "Wronge server type (`0` for aborting zone creation)"
-		read -p "Pleas enter the the server typre [master|slave]: " type
-	fi
-done
+if [ $zonev4 == "yes" ]
+then
+    if [ $type == "master" ]
+    then
+        # Edit Zone
+        sed -i -e "s/example.com/$domain/g" ./master.sql
+        sed -i -e "s/placeNS0.com/$masterip/g" ./master.sql
+        sed -i -e "s/placeNS1.com/$slaveip/g" ./master.sql
+        sed -i -e "s/placeA/$ARecord/g" ./master.sql
+        # Create Zone
+        mysql -u root -p$MYSQL_ROOT_PASSWORD < ./master.sql
+        echo "mMster zone IPv4 created for $domain"
+        # Reset Zone
+        sed -i -e "s/$domain/example.com/g" ./master.sql
+        sed -i -e "s/$masterip/placeNS0.com/g" ./master.sql
+        sed -i -e "s/$slaveip/placeNS1.com/g" ./master.sql
+        sed -i -e "s/$ARecord/placeA/g" ./master.sql
+    elif [ $type == "slave" ]
+    then
+        # Edit Zone
+        sed -i -e "s/example.com/$domain/g" ./slave.sql
+        sed -i -e "s/placeNS0.com/$masterip/g" ./master.sql
+        # Create Zone
+        mysql -u root -p$MYSQL_ROOT_PASSWORD < ./slave.sql
+        echo "Slave zone IPv4 created for $domain"
+        # Reset Zone
+        sed -i -e "s/$domain/example.com/g" ./slave.sql
+        sed -i -e "s/$masterip/placeNS0.com/g" ./master.sql
+    fi
+else
+    echo "No zone created" 
+fi 
