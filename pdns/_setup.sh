289feed988a548550c376977ac65b35d7b64342a
iGlaedr <jannik.tom@gmail.com>
2018-04-23 08:39:03 +0200

    Update _setup.sh
    Automatically no to the pdns-mysql-backend configurator
    Read passwords stealthy
    Comments

--- pdns/_setup.sh
+++ pdns/_setup.sh
@@ -2,28 +2,46 @@
 
 #Get configuration from user
 echo 	"Script to setup powrdns [master|slave] nameserver"
-read -p "Pleas enter the master IP" masterip
-read -p "Pleas enter the slave IP" slaveip
-read -p "Pleas enter the domain" domain
-read -p "Pleas enter the A Records IP" ARecord
-read -p "Pleas enter the your new MYSQL Root password" MYSQL_ROOT_PASSWORD
-read -p "Pleas enter the your new DB user" MYSQL_DB_USER
-read -p "Pleas enter the the new password for the DB user" MYSQL_DB_PASSWORD
-read -p "Pleas enter the the server typre [master|slave]("0" for aborting zone creation)" type
+read -p "Pleas enter the master IP: " masterip
+read -p "Pleas enter the slave IP: " slaveip
+read -p "Pleas enter the domain: " domain
+read -p "Pleas enter the A Records IP: " ARecord
+read -p "Pleas enter the your new MYSQL Root password:  " -r -s MYSQL_ROOT_PASSWORD && echo ""
+read -p "Pleas enter the your new DB user: " MYSQL_DB_USER
+read -p "Pleas enter the the new password for the DB user: " -r -s MYSQL_DB_PASSWORD && echo ""
+read -p "Pleas enter the the server typre [master|slave]("0" for aborting zone creation): " type
 
-# Install packages
-apt update && apt install -y mariadb-client mariadb-server pdns-server pdns-backend-mysql
+# Install expect needed for interaction between script an UIs
+apt update && apt -y install expect
+
+# Install packages 
+# Automatically yes to installation and no to the pdns-mysql-backend configurator 
+install_packages=$(expect -c "
+spawn apt install mariadb-client mariadb-server pdns-server pdns-backend-mysql
+expect \"Do you want to continue? \"
+send \"y\r\"
+expect \"Package configuration\"
+sleep 1
+send \(\"\{TAB\}\"\)
+sleep 1
+send \(\"\{Enter\}\"\)
+expect eof
+")
+echo "$install_packages"
 
 # Setup mariadb
-apt -y install expect
-# Set password
+# Set root password
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
 send \"$MYSQL\r\"
 expect \"Change the root password?\"
-send \"n\r\"
+send \"y\r\"
+expect \"New password: \"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+expect \"Re-enter new password: \"
+send \"$MYSQL_ROOT_PASSWORD\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"
@@ -35,16 +53,19 @@ send \"y\r\"
 expect eof
 ")
 echo "$SECURE_MYSQL"
+# Remove expect and its dependencies
 apt -y purge expect
 apt -y autoremove 
 
 # Configure hosts file
+# Needed to ensure that name-server can see each other even if domain is not up
 echo "
 $masterip 	ns0.$domain
 $slaveip 	ns1.$domain" >> /etc/hosts
 
-# Configuring pdns server
+# Move original conf to backup
 mv /etc/powerdns/pdns.conf /etc/powerdns/_orgi.pdns.conf
+# Create config files
 cp ./pdns.conf /etc/powerdns/pdns.conf
 cp ./pdns.gmysql.conf /etc/powerdns/pdns.d/pdns.gmysql.conf 
 
@@ -55,7 +76,7 @@ sed -i -e "s/powerdns_user/$MYSQL_DB_USER/g" /etc/powerdns/pdns.conf
 sed -i -e "s/powerdns_user_password/$MYSQL_DB_PASSWORD/g" /etc/powerdns/pdns.conf
 
 # Create database
-mysql -u root -p123.qwe < ./schema.sql
+mysql -u root -p$MYSQL_ROOT_PASSWORD < ./schema.sql
 
 # Create zone
 while [ $type !=  "0" ]
@@ -68,7 +89,7 @@ do
 		sed -i -e "s/placeNS1.com/$slaveip/g" ./master.sql
 		sed -i -e "s/placeA/$ARecord/g" ./master.sql
 		# Create Zone
-		mysql -u root -p123.qwe < ./master.sql
+		mysql -u root -$MYSQL_ROOT_PASSWORD < ./master.sql
 		# Reset Zone
 		sed -i -e "s/$domain/example.com/g" ./master.sql
 		sed -i -e "s/$masterip/placeNS0.com/g" ./master.sql
@@ -79,11 +100,11 @@ do
 		# Edit Zone
 		sed -i -e "s/example.com/$domain/g" ./slave.sql
 		# Create Zone
-		mysql -u root -p123.qwe < ./slave.sql
+		mysql -u root -$MYSQL_ROOT_PASSWORD < ./slave.sql
 		# Reset Zone
 		sed -i -e "s/$domain/example.com/g" ./slave.sql
 	else
 		echo "Wronge server type (`0` for aborting zone creation)"
-		read -p "Pleas enter the the server typre [master|slave]" type
+		read -p "Pleas enter the the server typre [master|slave]: " type
 	fi
 done
